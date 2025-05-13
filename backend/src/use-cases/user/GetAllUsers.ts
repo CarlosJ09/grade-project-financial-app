d3843e9f58cedf254d9eebc66aaa0a2d6b4d9296
CarlosJ09 <c.jimenez@dominicanadeseguros.com>
2025-05-12 22:39:21 -0400

    Add backend docker file

--- backend/src/use-cases/user/GetAllUsers.ts
+++ backend/src/use-cases/user/GetAllUsers.ts
@@ -2,7 +2,7 @@ import { UserRepository } from "@/domain/interfaces/UserRepository";
 import { User } from "@/domain/entities/User";
 
 export class GetAllUsers {
-    constructor(private readonly userRepository: UserRepository) {}
+    constructor(private userRepository: UserRepository) {}
     
     async execute(): Promise<User[]> {
         return this.userRepository.findAll();
