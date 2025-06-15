c181c0c2efc508fe0541d136af4958780978bb7d
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2025-06-14 22:32:39 -0400

    Create sign-in page from wireframe

--- frontend/src/app/(auth)/sign-in.tsx
+++ frontend/src/app/(auth)/sign-in.tsx
@@ -1,22 +1,50 @@
-import { CustomTextInput } from "@/components/ui/CustomTextInput";
+import { Button } from "@/components/ui/Button";
+import { TextInput } from "@/components/ui/TextInput";
+import Checkbox from 'expo-checkbox';
 import { Link } from "expo-router";
-import { Button, Text } from "react-native";
+import { useState } from "react";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
+
+    const [errors, setErrors] = useState<{
+        cedula: string;
+        password: string;
+    }>({
+        cedula: '',
+        password: '',
+    });
+
     return (
-        <SafeAreaView className="flex-1 items-center justify-center">
-            <Text className="text-2xl font-bold">SignIn</Text>
+        <SafeAreaView className="flex-1 bg-white dark:bg-slate-600">
+            <View className="flex-1 justify-center p-8">
 
-            <CustomTextInput label="Email" error="Error" />
-            <CustomTextInput label="Password" error="Error" />
+                <View className="w-full gap-8 mb-8">
+                    <TextInput label="Cédula" placeholder="000-0000000-0" keyboardType="numeric" error={errors.cedula} />
 
-            <Button title="SignIn" onPress={() => { }} />
+                    <TextInput label="Contraseña" placeholder="*********" keyboardType="visible-password" error={errors.password} />
 
-            <Link href="/app/(auth)/sign-up" asChild>
-                <Text className="text-blue-500">SignUp</Text>
-            </Link>
-        </SafeAreaView>
+                    <View className="flex-row items-center gap-2">
+                        <Checkbox />
+                        <Text>Recordar Sesión</Text>
+                    </View>
+                </View>
+
+                <Button title="Iniciar Sesión" onPress={() => { }} />
+
+                <Link href="/app/(auth)/forgot-password" asChild>
+                    <Text className="mt-4">Olvidé mi contraseña</Text>
+                </Link>
+
+                <View className="flex-row items-center justify-center gap-2 mt-16">
+                    <Text>No tienes una cuenta?</Text>
+                    <Link href="/app/(auth)/sign-up" asChild>
+                        <Text>Registrate</Text>
+                    </Link>
+                </View>
+            </View>
+        </SafeAreaView >
     );
 };
 
---
+++ frontend/src/components/ui/TextInput.tsx
@@ -0,0 +1,25 @@
+import { TextInput as RNTextInput, TextInputProps as RNTextInputProps, Text, View } from "react-native";
+
+interface TextInputProps extends RNTextInputProps {
+    label: string;
+    error?: string;
+    containerClassName?: string;
+    inputClassName?: string;
+    labelClassName?: string;
+}
+
+export const TextInput = (props: TextInputProps) => {
+    return (
+        <View className={`gap-2 ${props.containerClassName}`}>
+            <Text className={`text-sm font-medium ${props.labelClassName}`}>{props.label}</Text>
+
+            <View className={`w-full border border-gray-200 rounded-xl p-2 ${props.inputClassName}`}>
+                <RNTextInput {...props} />
+            </View>
+
+            {props.error && <Text>{props.error}</Text>}
+        </View>
+    );
+};
+
+export default TextInput;
