3d50a2628a2321a207cfcedb5638fcf86f214b5a
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2025-09-16 16:43:49 -0400

    Change package manager from npm to pnpm

--- frontend/src/interceptor/core-api.ts
+++ frontend/src/interceptor/core-api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.EXPO_PUBLIC_CORE_API_URL}/api`,
+  baseURL: `${process.env.EXPO_PUBLIC_API_URL}/api`,
   headers: {
     'Content-type': 'application/json',
   },
